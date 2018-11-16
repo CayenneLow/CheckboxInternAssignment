5f0c6bd6b7cf9dece6f5335781b83a661ee488a3
Khye Ean <lowkhyeean@gmail.com>
2018-11-16 12:38:20 +0800

    added hover

--- src/components/table.js
+++ src/components/table.js
@@ -1,5 +1,7 @@
 import React from 'react'
-import {Table, Button, Label, Glyphicon} from 'react-bootstrap';
+import {Table, Button, Label, Glyphicon, Popover, OverlayTrigger} from 'react-bootstrap';
+
+
 
 export default function PlanetTable(props) {
 	return(
@@ -25,17 +27,30 @@ export default function PlanetTable(props) {
           </tr>
         </thead>
         <tbody>
-          {
-            // maps each item in props to a row/entry in the table
-          	props.data.map((planet,index) => (
-	            <tr key={index} style={{fontSize:15}}>
-		            <td> {index + 1} </td>
-		            <td> {planet.name} </td>
-		            <td> {planet.population} </td>
-		            <td> {planet.terrain} </td>
-	            </tr>
-          	))
-          }
+            {
+              // maps each item in props to a row/entry in the table
+            	props.data.map((planet,index) => (
+
+      	            <tr key={index} style={{fontSize:15}}>
+      		            <td> {index + 1} </td>
+                      <OverlayTrigger
+                        trigger={['hover', 'focus']}
+                        placement="right"
+                        overlay= 
+                        <Popover id="popover-trigger-hover-focus" title={planet.name}>
+                          <strong>Diameter: &nbsp;</strong>{planet.rotation_period} <br/>
+                          <strong>Climate: &nbsp;</strong>{planet.climate} <br/>
+                          <strong>Gravity: &nbsp;</strong>{planet.gravity} <br/>
+                          <strong>Surface Water: &nbsp;</strong>{planet.surface_water} <br/>
+                        </Popover>
+                      >
+                      <td> {planet.name} </td>
+                      </OverlayTrigger>
+      		            <td> {planet.population} </td>
+      		            <td> {planet.terrain} </td>
+      	            </tr>
+            	))
+            }
       	</tbody>
       </Table>
     )
